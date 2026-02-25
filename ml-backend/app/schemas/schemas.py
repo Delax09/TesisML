e3132c1d90b02de0cf1ac29db15a5fdf5ab91183
Fabian Mejias <fabian.mejiast@outlook.com>
2026-02-25 16:49:12 -0300

    Crud realizado de Sectores y Empresas El CRUD de Sector funciona Voy a ver como refactorizarlo para que sea escalable

--- ml-backend/app/schemas/schemas.py
+++ ml-backend/app/schemas/schemas.py
@@ -1,12 +1,49 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from datetime import datetime
+
+# Schema sector 
+class SectorBase(BaseModel):
+    NombreSector: str
+
+class SectorCreate(SectorBase):
+    pass
+
+class SectorUpdate(BaseModel):
+    NombreSector: Optional[str] = None
+
+    model_config = {"from_attributes": True}
+
+class SectorOut(SectorBase):
+    IdSector: int
+
+    model_config = {
+        "from_attributes": True
+    }
 
 # Schema para mostrar la información de una empresa
-class EmpresaOut(BaseModel):
-    IdEmpresa: int
+class EmpresaBase(BaseModel):
     Ticket: str
     NombreEmpresa: str
     IdSector: int
 
-    class Config:
-        orm_mode = True # Esto le dice a Pydantic que lea el objeto de SQLAlchemy
+class EmpresaCreate(EmpresaBase):
+    pass
+
+class EmpresaUpdate(BaseModel):
+    Ticket: Optional[str] = None
+    NombreEmpresa: Optional[str] = None
+    IdSector: Optional[int] = None
+
+class EmpresaOut(EmpresaBase):
+    IdEmpresa: int 
+    FechaAgregado: datetime 
+
+    model_config = {"from_attributes": True}
+
+class EmpresaUpdate(BaseModel):
+    Ticket: Optional[str] = None
+    NombreEmpresa: Optional[str] = None
+    IdSector: Optional[int] = None
+
+    model_config = {"from_attributes": True}
