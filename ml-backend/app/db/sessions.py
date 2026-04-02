fab1c507530b427b526f38856f265896da3ee40f
Fabian Mejias <fabian.mejiast@outlook.com>
2026-04-01 23:58:29 -0300

    Actualizacion en session

--- ml-backend/app/db/sessions.py
+++ ml-backend/app/db/sessions.py
@@ -1,19 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base # <- Importación recuperada
+from sqlalchemy.ext.declarative import declarative_base
 from app.core.config import settings
 
-# Forzamos la desactivación de prepared statements para evitar errores de duplicidad
+# Forzamos la desactivación de prepared statements en SQLAlchemy y Psycopg3
 engine = create_engine(
     settings.DATABASE_URL,
     execution_options={
-        "prepared_statement_cache_size": 0
+        "prepared_statement_cache_size": 0 # Apaga el caché de SQLAlchemy
+    },
+    connect_args={
+        "prepare_threshold": None
     }
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# ESTA ES LA LÍNEA CLAVE QUE FALTABA
 Base = declarative_base()
 
 def get_db():
