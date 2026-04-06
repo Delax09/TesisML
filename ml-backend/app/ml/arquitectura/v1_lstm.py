9cfdbb8dbe22ba7b704c1528785d5a1e173d927b
Fabian Mejias <fabian.mejiast@outlook.com>
2026-04-06 12:27:43 -0400

    Aplique GRU a los modelos de LSTM

--- ml-backend/app/ml/arquitectura/v1_lstm.py
+++ ml-backend/app/ml/arquitectura/v1_lstm.py
@@ -1,25 +1,31 @@
 import torch
 import torch.nn as nn
 
-class ModeloLSTM_v1(nn.Module):
+class ModeloLSTM_v1(nn.Module): # Mantenemos el nombre para no romper los imports
     def __init__(self, num_features):
         super(ModeloLSTM_v1, self).__init__()
-        self.lstm = nn.LSTM(input_size=num_features, hidden_size=32, batch_first=True)
+        
+        self.gru = nn.GRU(input_size=num_features, hidden_size=32, batch_first=True)
         self.dropout = nn.Dropout(0.4) 
         self.fc1 = nn.Linear(32, 16)
         self.relu = nn.ReLU()
         
+        # --- RED MULTI-TAREA ---
         self.cabeza_regresion = nn.Linear(16, 1)     
         self.cabeza_clasificacion = nn.Linear(16, 1) 
 
     def forward(self, x):
-        lstm_out, _ = self.lstm(x)
-        ultimo_dia = lstm_out[:, -1, :] 
+        # La GRU procesa la secuencia temporal
+        gru_out, _ = self.gru(x)
+        
+        # Extraemos el último día de la secuencia
+        ultimo_dia = gru_out[:, -1, :] 
+        
         x = self.dropout(ultimo_dia)
         x = self.relu(self.fc1(x))
         
         precio_predicho = self.cabeza_regresion(x)
-        direccion_predicha = self.cabeza_clasificacion(x)
+        direccion_predicha = self.cabeza_clasificacion(x) # Sin sigmoide
         
         return precio_predicho, direccion_predicha
 
--- ml-backend/app/ml/arquitectura/v2_bidireccional.py
+++ ml-backend/app/ml/arquitectura/v2_bidireccional.py
@@ -4,22 +4,27 @@ import torch.nn as nn
 class ModeloBidireccional_v2(nn.Module):
     def __init__(self, num_features):
         super(ModeloBidireccional_v2, self).__init__()
-        self.lstm = nn.LSTM(input_size=num_features, hidden_size=32, batch_first=True, bidirectional=True)
+        
+        self.gru = nn.GRU(input_size=num_features, hidden_size=32, batch_first=True, bidirectional=True)
         self.dropout = nn.Dropout(0.4)
+        
+        # Como es bidireccional, el hidden_size se multiplica por 2 (32 * 2 = 64)
         self.fc1 = nn.Linear(64, 16) 
         self.relu = nn.ReLU()
         
+        # --- RED MULTI-TAREA ---
         self.cabeza_regresion = nn.Linear(16, 1)
         self.cabeza_clasificacion = nn.Linear(16, 1)
 
     def forward(self, x):
-        lstm_out, _ = self.lstm(x)
-        ultimo_dia = lstm_out[:, -1, :]
+        gru_out, _ = self.gru(x)
+        ultimo_dia = gru_out[:, -1, :]
+        
         x = self.dropout(ultimo_dia)
         x = self.relu(self.fc1(x))
         
         precio_predicho = self.cabeza_regresion(x)
-        direccion_predicha = self.cabeza_clasificacion(x)
+        direccion_predicha = self.cabeza_clasificacion(x) # Sin sigmoide
         
         return precio_predicho, direccion_predicha
 
